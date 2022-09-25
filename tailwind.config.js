cf6cb9f739c4e29d1d7f0e0c79d6487b450c2ae8
Moe <mohammed@dnsnetworks.com>
2022-09-25 13:13:31 -0400

    Added styles to tailwind.config and globals.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,26 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: '#0C0C0C',
+        pink: '#CE1982',
+        blue: '#026ABC',
+        yellow: '#EBC967',
+        lightBlue: '#8FC1E8',
+      },
+      fontFamily: {
+        mainFont: ['Montserrat', 'sans-serif'],
+      },
+    },
+    screens: {
+      xs: '480px',
+      ss: '620px',
+      sm: '768px',
+      md: '1060px',
+      lg: '1200px',
+      xl: '1700px',
+    },
   },
   plugins: [],
 };
