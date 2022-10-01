84e82928e515bf5a27e6a84c21969fe3b548e31a
joshuapadronuy <joshuapadronuy@gmail.com>
2022-09-30 20:53:24 -0700

    added test page for testing

--- components/BubbleTags.tsx
+++ components/BubbleTags.tsx
@@ -1,16 +1,40 @@
 import BubbleTag, { BUBBLE_TAG_TYPE_CLASSES } from './BubbleTag';
 import bubbleTagStyles from '../styles/BubbleTag.module.css';
+import { v4 as uuidv4 } from 'uuid';
+
 
 interface Tags {
-  tags: string[]
+  tags: string[],
+  bubbleTagType: BUBBLE_TAG_TYPE_CLASSES,
+  index: number,
+  alternativeTagType: BUBBLE_TAG_TYPE_CLASSES,
 }
 
-const BubbleTags = ({ tags }: Tags) => {
+/*
+  BubbleTags component's main use is to take in an array and display all of its element as a tag
+
+  To use this component in different pages, BUBBLE_TAG_TYPE_CLASSES must 
+  also be imported together with this component since bubbleTagType prop 
+  takes in a BUBBLE_TAG_TYPE_CLASSES
+
+  index and alternativeType props are optional and are used to select a 
+  single element and change its color to the alternative one 
+*/
+const BubbleTags = ({ tags, bubbleTagType, index, alternativeTagType }: Tags) => {
+  let selectedTagType;
   return (
     <div className={bubbleTagStyles.container}>
-      {tags.map((tag: string) => (
-        <BubbleTag tag={tag} bubbleTagType={BUBBLE_TAG_TYPE_CLASSES.blue} />
-      ))}
+      {tags.map((tag: string, i: number) => {
+
+        //condition to handle cases if the component involves the index prop
+        if(i == index){
+          selectedTagType = alternativeTagType
+        } else {
+          selectedTagType = bubbleTagType
+        }
+
+        return <BubbleTag key={uuidv4()} tag={tag} bubbleTagType={selectedTagType} />
+        })}
     </div>
   );
 };
