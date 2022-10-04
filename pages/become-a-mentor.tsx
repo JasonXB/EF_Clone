f97bbef4294a5d8391588ca8be10077f1eacbe1f
JasonXB <jasonxportfolio@gmail.com>
2022-10-03 22:36:15 -0400

    Implemented static rendering for the "become a mentor" page

--- pages/become-a-mentor.tsx
+++ pages/become-a-mentor.tsx
@@ -1,7 +1,13 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import { GetStaticProps } from 'next';
 
-const becomeAMentor: NextPage = ({}) => {
+// Statically renders page and sets props equal to an empty object
+export const getStaticProps: GetStaticProps = async (context) => {
+  return { props: {} };
+};
+
+const becomeAMentor: NextPage = (props) => {
   // Will map over this list to generate JSX in order to avoid retyping classes
   const content = [
     {
