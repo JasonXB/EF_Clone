45376b47967afe38ccd08b936ea2a5eadc23e7e9
Matt Engerer <matt.engerer@aol.com>
2022-10-04 20:50:31 -0400

    test image and imports removed from index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
-import Avatar from '../components/avatar/avatar';
-
-import Cat from '../assets/cat.jpeg';
-
 const Home: NextPage = ({}) => {
   return (
     <div>
@@ -14,9 +10,6 @@ const Home: NextPage = ({}) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>Home Page - Hello world!</h1>
-      <div>
-        <Avatar imgLocation={Cat} displaySize="small" personsName="Elon Musk" />
-      </div>
     </div>
   );
 };
