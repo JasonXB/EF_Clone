752759ebd113b090dd6517c1651700ba6f284aca
Kelsey H <kels-hall23@hotmail.com>
2022-10-05 12:08:32 -0400

    basic open close toggle

--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -2,16 +2,19 @@ import NavbarLink from './NavbarLink';
 import EfLogo from './EfLogo';
 import Button, { buttonVariants } from '../buttons/reusuable-buttons';
 import Link from 'next/link';
+import { useState } from 'react';
 
 const MainLinks = () => (
-  <>
-    <NavbarLink url={'/become-a-mentor'} name={'Become a Mentor'} />
-    <NavbarLink url={'/find-a-mentor'} name={'Find a Mentor'} />
-    <NavbarLink url={'/about-us'} name={'About Us'} />
-  </>
+  <div className="w-full lg:inline-flex lg:w-auto">
+    <div className="flex flex-col items-start w-full lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto lg:items-center lg:h-auto">
+      <NavbarLink url={'/become-a-mentor'} name={'Become a Mentor'} />
+      <NavbarLink url={'/find-a-mentor'} name={'Find a Mentor'} />
+      <NavbarLink url={'/about-us'} name={'About Us'} />
+    </div>
+  </div>
 );
 const LoginSignup = () => (
-  <>
+  <div className="flex flex-col items-start w-full mr-16 lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto lg:items-center lg:h-auto">
     {/*These links are made to look like buttons */}
     <Link href={'/login'}>
       <a className={buttonVariants.secondary}>Login</a>
@@ -19,13 +22,14 @@ const LoginSignup = () => (
     <Link href={'/signup'}>
       <a className={buttonVariants.primary}>Sign Up</a>
     </Link>
-  </>
+  </div>
 );
 
 const ProfileNavMenu = () => <div>Temp</div>;
 
 const Navbar = () => {
   let notLoggedIn = true; //temp for testing, will switch to auth
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
       {/* Empowered Futures Logo / Link to the Homepage */}
@@ -33,15 +37,23 @@ const Navbar = () => {
       <nav className="flex flex-wrap items-center pt-5 border-b shadow-md border-slate-200">
         <EfLogo alt={'Empowered Futures Logo'} />
         {/*Desktop menu */}
-        <div className="w-full lg:inline-flex lg:w-auto">
-          <div className="flex flex-col items-start w-full lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto lg:items-center lg:h-auto">
-            <MainLinks />
-          </div>
-        </div>
-        <div className="flex flex-col items-start w-full mr-16 lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto lg:items-center lg:h-auto">
+        <div className="hidden lg:block">
+          <MainLinks />
+
           {notLoggedIn ? <LoginSignup /> : <ProfileNavMenu />}
         </div>
         {/*Mobile + tablet */}
+
+        <Button className="lg:hidden" clickHandler={() => setIsOpen(!isOpen)}>
+          =
+        </Button>
+
+        {isOpen ? (
+          <>
+            <MainLinks />
+            {notLoggedIn ? <LoginSignup /> : <ProfileNavMenu />}
+          </>
+        ) : null}
       </nav>
     </>
   );
