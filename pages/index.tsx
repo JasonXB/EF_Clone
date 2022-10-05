279f2a27f27133edeff32f8e82278cd968dd3b80
Kelsey H <kels-hall23@hotmail.com>
2022-10-04 20:57:30 -0400

    merge adjustments in foldering/linking

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from 'next';
 import Link from 'next/link';
-
-import Layout from '../components/Layout';
+import Layout from '../src/components/Layout';
 
 const Home: NextPage = ({}) => {
   return (
