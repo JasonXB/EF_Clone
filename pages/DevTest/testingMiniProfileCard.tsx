4daa433af7ac16ddf170975eb6fd5c129ef8a39b
Jamie Pantling <99222790+jamiepantling@users.noreply.github.com>
2022-10-11 18:38:56 -0400

    add percentagebar, update interface, add variables to props

--- pages/DevTest/testingMiniProfileCard.tsx
+++ pages/DevTest/testingMiniProfileCard.tsx
@@ -5,7 +5,7 @@ import MiniProfileCard from '../../src/components/MiniProfileCard';
 const testMiniProfile: NextPage = ({}) => {
   return (
     <div>
-      <MiniProfileCard name="Kelsey" location="canada" job="Developer" />
+      <MiniProfileCard name="Kelsey" location="canada" job="Developer" compatibilityPercent={63} tags={["management", "design"]} />
     </div>
   );
 };
--- src/components/MiniProfileCard.tsx
+++ src/components/MiniProfileCard.tsx
@@ -1,28 +1,27 @@
 import BubbleTags from './BubbleTags';
 import Avatar from './avatar/avatar';
-import tempImage from '../../src/assets/cat.jpeg';
+import tempImage from './assets/cat.jpeg';
 import { BUBBLE_TAG_TYPE_CLASSES } from './BubbleTag';
 import style from '../../styles/MiniProfileCard.module.css';
+import PercentageBar from './percentBar/percent-bar'
 
 interface MentorsInfoProps {
   name: string;
   location: string;
   job: string;
   tags?: string[]; //temp optional, remove ? once implemented
-  compatibilityPercent?: number; //temp optional, remove ? once implemented
+  compatibilityPercent: number;
 }
 
 const MiniProfileCard = ({ ...mentorsInfo }: MentorsInfoProps) => {
-  const { name, location, job } = mentorsInfo;
-  const arrTags = ['management', 'design']; //should also be passed in, add to above deconstruction value
-  const personsName = 'Bilbo Baggins'; //this should be passed in from mentorsInfo
+  const { name, location, job, tags, compatibilityPercent } = mentorsInfo;
 
   return (
     <div className={`${style.miniProfileCard} flex`}>
       <Avatar
         imgLocation={tempImage}
         displaySize="mediumLarge"
-        personsName={personsName}
+        personsName={name}
       />
       <div className={`${style.infoContainer} font-mainFont pl-3`}>
         <h2 className={style.name}>{name}</h2>
@@ -30,12 +29,10 @@ const MiniProfileCard = ({ ...mentorsInfo }: MentorsInfoProps) => {
         <h4 className={style.locationJob}>{job}</h4>
         {/* Remove below div when <BubbleTags/> is working, and uncomment <BubbleTags/> */}
         <div className="my-4 h-7">[Bubble Tags]</div>
-        {/* <BubbleTags tags={arrTags} bubbleTagType={BUBBLE_TAG_TYPE_CLASSES.blue} /> */}
+        {/* <BubbleTags tags={tags} bubbleTagType={BUBBLE_TAG_TYPE_CLASSES.blue} /> */}
         <div className={style.compatibility}>
-          <h4>Compatibility</h4>
-          {/* Remove below div when <PercentBar/> is working, and uncomment <PercentBar/> */}
-          <div className="my-4 h-7">[Percent Bar]</div>
-          {/* <PercentBar /> */}
+          <h4 className={`${style.compatibility} pb-1`}>Compatibility</h4>
+          <PercentageBar percentage={compatibilityPercent} color="blue" showPercentageText={true}/>
         </div>
       </div>
     </div>
