bb9798a030db4e6828521a7680a4b723c15ff271
joshuapadronuy <joshuapadronuy@gmail.com>
2022-10-13 09:04:07 -0700

    added 1 more test case for test page

--- pages/test.jsx
+++ pages/test.jsx
@@ -9,12 +9,20 @@ const arr = ['tomato', 'lettuce', 'cat', 'tomato', 'lettuce', 'cat'];
 const test = () => {
   console.log(tailwind.theme.extend.colors);
   return (
-    <BubbleTags
-      tags={arr}
-      bubbleTagType={BUBBLE_TAG_TYPE_CLASSES.primaryLight}
-      index={0}
-      alternativeTagType={BUBBLE_TAG_TYPE_CLASSES.primaryShaded}
-    />
+    <>
+      <p>first test</p>
+      <BubbleTags
+        tags={arr}
+        bubbleTagType={BUBBLE_TAG_TYPE_CLASSES.primaryLight}
+        index={0}
+        alternativeTagType={BUBBLE_TAG_TYPE_CLASSES.primaryShaded}
+      />
+      <p>second test</p>
+      <BubbleTags
+        tags={arr}
+        bubbleTagType={BUBBLE_TAG_TYPE_CLASSES.primaryLight}
+      />
+    </>
   );
 };
 
