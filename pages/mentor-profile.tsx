29a1391995e848b069c65cee3b1057ee4f3bbbcc
Matt Engerer <matt.engerer@aol.com>
2022-10-14 01:00:13 -0400

    moved files around, minor changes/comments

--- pages/mentor-profile.tsx
+++ pages/mentor-profile.tsx
@@ -1,45 +1,12 @@
 import type { NextPage } from 'next';
 import MentorProfileSection from '../src/components/mentorProfile/MentorProfileMain';
 import Layout from '../src/components/Layout';
-import { linkedInIcon } from '../src/components/mentorProfile/ProfileIcons';
-import { twitterIcon } from '../src/components/mentorProfile/ProfileIcons';
-import hiba from '../src/components/assets/hiba.png'
-
-
-/* Since the style of SM icons in the Social Media Links component did not match those in the Adobe XD, 
-I pulled different ones temporarily from Bootstrap icons. However, can easily switch to those already 
-found in the component. :) 
-*/
-const dummyMentors = [
-  {
-    id: 1,
-    name: 'Hiba Badran',
-    title: 'Founder of Empowered Futures',
-    // Change this 
-    avatar: hiba,
-    socialMediaIcons: [
-      {svg: linkedInIcon, url: 'http://linkedin.com' },
-      {svg: twitterIcon, url: 'http://twitter.com' }
-    ], 
-    location: 'Calgary, Canada',
-    responseTime: 'Usually responds within 1 day',
-    skills: ['Entrepeneurship', 'Management', 'Problem Solving'],
-    percentBarSkills: [
-      {name: 'Grow a Business', percentage: 90 }, 
-      {name: 'Adobe XD', percentage: 20 },
-      {name: 'Networking', percentage: 40 },
-      {name: 'Mindset Coaching', percentage: 70 },
-      {name: 'Figma', percentage: 100 },
-    ],
-    about: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam aut voluptatibus, corrupti in placeat libero necessitatibus numquam architecto corporis? Quo nulla nisi aperiam maiores explicabo inventore ex quaerat voluptates dolor?'
-  }
-]
+import { dummyMentorProfiles } from '../src/components/mentorProfile/dummyMentorProfiles'
 
 // Temporarily displaying dummyMentor data (from above)
-const {id, name, title, avatar, socialMediaIcons,  location, responseTime, skills, about, percentBarSkills} = dummyMentors[0]
+const {id, name, title, avatar, socialMediaIcons,  location, responseTime, skills, about, percentBarSkills} = dummyMentorProfiles[0]
 
 const mentorProfile: NextPage = () => {
-
   return (
 <Layout>
 
@@ -49,9 +16,6 @@ const mentorProfile: NextPage = () => {
 {/* Similar Mentors Component */}
 
 </Layout>
-
-    
-
   );
 };
 
