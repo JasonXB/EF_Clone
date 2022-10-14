8ae7af326387fc1bbfaf3588796a5d853e3f5dbc
puneet2121 <bedipuneet21@gmail.com>
2022-10-14 01:03:56 -0600

    Work on profile setting page

--- pages/profile-settings.tsx
+++ pages/profile-settings.tsx
@@ -34,7 +34,7 @@ const ProfileSettings: NextPage = ({}) => {
         <svg
           className="h-full w-full text-gray-300 mb-5"
           fill="currentColor"
-          viewBox="0 0 24 24"
+          viewBox="0 0 23 24"
         >
           <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
         </svg>
