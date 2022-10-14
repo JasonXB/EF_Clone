5779cc2a1c5a83bdf41a05b1f8650d4e3206ac55
Jamie Pantling <99222790+jamiepantling@users.noreply.github.com>
2022-10-14 13:29:21 -0400

    fix button by adding clickHandler

--- pages/congratulations-mentee.tsx
+++ pages/congratulations-mentee.tsx
@@ -1,17 +1,23 @@
 import type { NextPage } from 'next';
-import Link from 'next/link'
-
+import Link from 'next/link';
+import Router from 'next/router';
 import Layout from '../src/components/Layout';
 import style from '../styles/congratulations-mentee.module.css';
 import Image from 'next/image';
 import fireworks from '../src/components/assets/fireworks.png';
 import Button from '../src/components/buttons/reusable-buttons';
+import { ReactEventHandler } from 'react';
 
 const congratulationsMentee: NextPage = ({}) => {
+  const clickHandler: ReactEventHandler = () => {
+    Router.push('/');
+  };
   return (
     <Layout>
       <div className="flex flex-col md:flex-row justify-center items-center md:items-start">
-        <div className={`${style.imageContainer} flex justify-center items-center md:py-18`}>
+        <div
+          className={`${style.imageContainer} flex justify-center items-center md:py-18`}
+        >
           <Image alt="illustration of fireworks" src={fireworks} />
         </div>
         <div className=" font-mainFont text-center w-ss px-3 md:py-20">
@@ -26,7 +32,15 @@ const congratulationsMentee: NextPage = ({}) => {
             <br /> Once the Mentor has accepted your request, your can book a
             meeting with them.
           </p>
-          <Link href={"/"}><Button variant="primary" disabled={false}>Go to Homepage</Button></Link>
+          <Link href="/">
+            <Button
+              variant="primary"
+              clickHandler={clickHandler}
+              disabled={false}
+            >
+              Go to Homepage
+            </Button>
+          </Link>
         </div>
       </div>
     </Layout>
