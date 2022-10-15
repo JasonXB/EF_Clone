1c139355f176be505c1d290858ec0a9ffe0ccf8f
Jamie Pantling <99222790+jamiepantling@users.noreply.github.com>
2022-10-15 16:18:26 -0400

    fix orphan words

--- pages/congratulations-mentee.tsx
+++ pages/congratulations-mentee.tsx
@@ -16,15 +16,15 @@ const congratulationsMentee: NextPage = ({}) => {
     <Layout>
       <div className="flex flex-col md:flex-row justify-center items-center md:items-start">
         <div
-          className={`${style.imageContainer} flex justify-center items-center md:py-18`}
+          className="object-contain max-w-[600px] flex justify-center items-center md:py-18"
         >
           <Image alt="illustration of fireworks" src={fireworks} />
         </div>
-        <div className=" font-mainFont text-center w-ss px-3 md:py-20">
-          <div className="font-bold text-primary-1 text-3xl sm:text-4xl lg:text-5xl ">
+        <div className=" font-mainFont flex flex-col justify-center items-center text-center w-ss px-3 md:py-20">
+          <div className="font-bold text-primary-1 text-3xl sm:text-4xl lg:text-5xl ss:w-[500px] md:w-full">
             CONGRATULATIONS ON BECOMING A MENTEE!
           </div>
-          <p className=" text-lg lg:text-2xl py-8">
+          <p className=" text-lg lg:text-2xl py-8 w-[330px] xs:w-[430px] lg:w-[600px]">
             Your Mentorship request has been sent to the selected Mentor! <br />
             <br />
             Check your emails for if the Mentor has accepted your mentorship
