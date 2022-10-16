03184d8bcc2826f15c19511b7c87afafb365d58c
joshuapadronuy <joshuapadronuy@gmail.com>
2022-10-16 08:29:01 -0700

    json setup done for unsplash

--- pages/DevTest/calendarPage.jsx
+++ pages/DevTest/calendarPage.jsx
@@ -1,9 +1,14 @@
+// import { mentorsData } from '../../src/util/mentors.json';
+import { fetchMentorPics } from '../../src/util/mentor-pics';
+import mentorsData from '../../src/util/mentors.json';
+
 //JSON file for list of mentors that the mentee applied to
 /*
 Once the testing is all good you may delete the following before merging:
 - uninstall unsplash JS dependency in the package.json
 - DevTest/calendarPage.jsx
-- src/util/mentorpics.js
+- src/util/mentor-pics.js
+- src/util/mentors.json
 
 The files that this branch is working on: 
 - Calender Component
@@ -16,14 +21,18 @@ The files that this branch is working on:
 To test this branch, please refer to:
 - DevTest/calendarPage.jsx
 */
-const myMentors = [
-  {
-    name: 'Sarah Geronimo',
-    profilePic: 'https://source.unsplash.com/gySMaocSdqs',
-  },
-];
 
-const calendarPage = () => {
+export async function getStaticProps(context) {
+  const mentorPics = await fetchMentorPics();
+  return {
+    props: { mentorPics: mentorsData },
+  };
+}
+
+const calendarPage = (props) => {
+  // console.log('mentorsData', mentorsData);
+  console.log(props.mentorPics[0]);
+  console.log(mentorsData);
   return <div>This is the calendar page tester</div>;
 };
 
