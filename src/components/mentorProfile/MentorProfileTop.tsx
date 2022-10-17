3fb260bef65fd2cd77957f9f501ecc8bb0e76d32
Matt Engerer <matt.engerer@aol.com>
2022-10-17 01:16:35 -0400

    more styling/breakpoints changes, percent bars width breakpoints adjusted

--- src/components/mentorProfile/MentorProfileTop.tsx
+++ src/components/mentorProfile/MentorProfileTop.tsx
@@ -16,16 +16,16 @@ const MentorProfileTop = ({
   return (
     <>
       {/* Left: Avatar and Request button */}
-      <div className="flex flex-col lg:flex-row xl:flex-row">
-        <div className="flex flex-col items-center w-full lg:items-start xl:items-start xl:w-2/5">
+      <div className="flex flex-col items-center md:items-start lg:items-start lg:flex-row xl:flex-row">
+        <div className="flex flex-col items-center w-3/5 md:items-center lg:items-center xl:items-start md:w-3/5 lg:w-4/5 xl:w-2/5">
           <Avatar personsName={name} imgLocation={avatar} displaySize="large" />
-          <Button className="w-2/4 p-2 mt-6 mb-6 xl:ml-10">Request</Button>
+          <Button className="px-8 mt-6 mb-6 text-xl w-3/8 xl:ml-14">Request</Button>
         </div>
 
         {/* Right: Name, location etc */}
-        <div className="w-full px-10 py-10 xl:px-0 xl:w-3/5 h-3/5">
+        <div className="w-full px-10 py-5 mt-4 xl:px-0 xl:w-3/5 h-3/5">
           {/* Mentor name */}
-          <h1 className="mb-10 text-5xl font-bold text-black xl:mb-2">
+          <h1 className="mb-6 text-5xl font-bold text-black xl:text-5xl lg:mb-2 xl:mb-2">
             {name}
           </h1>
           <h2 className="mb-2 text-lg">{title}</h2>
