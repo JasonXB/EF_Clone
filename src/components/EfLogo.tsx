cc0c77ff28418a8868e5493f7f40e61aff12bbde
Kelsey H <kels-hall23@hotmail.com>
2022-10-19 10:15:50 -0400

    fixed nav, updated logo

--- src/components/EfLogo.tsx
+++ src/components/EfLogo.tsx
@@ -1,23 +1,17 @@
 import Link from 'next/link';
 import { EfLogoProps } from '../interface/ef-logo-props.interface';
 import Image from 'next/image';
-import eflogo from './assets/eflogo.png';
- 
+import eflogo from '../assets/logo_black.png';
 
-const EfLogo = ({ alt, className}:  EfLogoProps) => {
+const EfLogo = ({ alt, className }: EfLogoProps) => {
   return (
-<>
- {/* Empowered Futures Logo / Link to the Homepage */}
- <Link href="/">
-    <a className="items-center block p-2 ml-6 mr-12 ">
-    <Image 
-      className={className}
-      alt={alt}
-      src={eflogo}
-      height={148}
-            />
-     </a>
-  </Link>
+    <>
+      {/* Empowered Futures Logo / Link to the Homepage */}
+      <Link href="/" aria-label="go back to homepage">
+        <a className="items-center block ">
+          <Image className={className} alt={alt} src={eflogo} priority />
+        </a>
+      </Link>
     </>
   );
 };
