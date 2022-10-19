7b150bb42726dd9774d77b5abf651cd4cc40216f
Jamie Pantling <99222790+jamiepantling@users.noreply.github.com>
2022-10-19 12:46:59 -0400

    amend data type

--- pages/DevTest/testingMentorCard.tsx
+++ pages/DevTest/testingMentorCard.tsx
@@ -3,26 +3,32 @@ import Head from 'next/head';
 import MentorCard from '../../src/components/MentorCard';
 
 const testMentorCard: NextPage = ({}) => {
+  const mockData = {
+    id: 1,
+    firstName: 'Elon',
+    fullName: 'Elon Musk',
+    location: 'Canada',
+    job: 'Founder, CEO at SpaceX',
+    //Should only show the first 2 skills on the card.
+    skills: [
+      ['Management', 90],
+      ['Communication', 90],
+      ['Design', 45],
+    ],
+    tags: ['management', 'design'],
+    aboutMentor:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris',
+  };
+
   return (
     <div>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
       </Head>
-      <MentorCard
-        id={1}
-        firstName="Elon"
-        fullName="Elon Musk"
-        location="Canada"
-        job="Founder, CEO at SpaceX"
-        //Should only show the first 2 skills on the card.
-        skills={[
-          ['Management', 90],
-          ['Communication', 90],
-          ["Design", 45]
-        ]}
-        tags={['management', 'design']}
-        aboutMentor="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris"
-      />
+      <MentorCard mentorsInfo={mockData}/>
     </div>
   );
 };
--- src/components/MentorCard.tsx
+++ src/components/MentorCard.tsx
@@ -1,28 +1,32 @@
 import BubbleTags from './BubbleTags';
 import Avatar from './avatar/avatar';
-import tempImage from './assets/cat.jpeg';
+import tempImage from '../assets/cat.jpeg';
 import { BUBBLE_TAG_TYPE_CLASSES } from './BubbleTag';
 import PercentageBar from './percentBar/percent-bar';
 import Button from './buttons/reusable-buttons';
 import Link from 'next/link';
 
-interface MentorsInfoProps {
-  id: number;
-  firstName: string;
-  fullName: string;
-  location: string;
-  job: string;
-  aboutMentor: string;
-  tags: string[]; 
-  skills: [string, number][];
-}
+// interface MentorsInfoProps {
+//   id: number;
+//   firstName: string;
+//   fullName: string;
+//   location: string;
+//   job: string;
+//   aboutMentor: string;
+//   tags: string[]; 
+//   skills: [string, number][];
+// }
 
-const MentorCard = ({ ...mentorsInfo }: MentorsInfoProps) => {
+// type DataType = {
+//   mentorsInfo: MentorsInfoProps
+// }
+
+const MentorCard = (mentorsInfo: any) => {
   const { id, firstName, fullName, location, job, tags, skills, aboutMentor } =
-    mentorsInfo;
+    mentorsInfo.mentorsInfo;
 
   return (
-    <div className="md:h-[264px] w-[550px] md:w-[850px] lg:w-[1011px] p-[28px] bg-white shadow-md rounded-[20px] flex">
+    <div className="w-[550px] md:w-[850px] md:h-[264px] lg:w-[1011px] p-[28px] bg-white shadow-md rounded-[20px] flex">
       <div className="md:h-[218px] w-[180px] flex flex-col justify-start md:justify-between items-center">
         <div className="h-[150px] w-[140px] overflow-hidden">
           <Avatar
@@ -35,7 +39,9 @@ const MentorCard = ({ ...mentorsInfo }: MentorsInfoProps) => {
         {/* Put in link to mentor page when available */}
 
         <Link href="">
-          <Button className="mt-[25px] md:m-1  text-[14px]" variant="secondary">View Profile</Button>
+          <Button className="mt-[25px] md:m-1  text-[14px]" variant="secondary">
+            View Profile
+          </Button>
         </Link>
       </div>
 
@@ -54,11 +60,13 @@ const MentorCard = ({ ...mentorsInfo }: MentorsInfoProps) => {
             <h4 className="text-lg leading-[22px] font-[600] pt-3">
               About {firstName}
             </h4>
-            <div className="text-xs leading-4 pt-[6px] overflow-hidden h-14 w-[300px] lg:w-[405px]">{aboutMentor}</div>
+            <div className="text-xs leading-4 pt-[6px] overflow-hidden h-14 w-[300px] lg:w-[405px]">
+              {aboutMentor}
+            </div>
           </div>
 
           <div className="h-full w-full flex flex-col justify-end items-left pb-4 md:pb-0 md:items-center gap-5">
-            {skills.slice(0, 2).map((skill: [string, number], i) => (
+            {skills.slice(0, 2).map((skill: [string, number], i: number) => (
               <div key={i}>
                 <div className="text-xl leading-6 mb-[18px]">{skill[0]}</div>
                 <PercentageBar
