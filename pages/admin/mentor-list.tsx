a72d10350a649447cce91b3e9a9f0069df1bf7f3
Shiho Nagano <ex3jsb.geneg@gmail.com>
2022-10-20 10:46:38 -0700

    added styles

--- pages/admin/mentor-list.tsx
+++ pages/admin/mentor-list.tsx
@@ -6,11 +6,11 @@ type MentorInfo = {
 
 const MentorList = ({ name, index }: MentorInfo) => {
   return (
-    <div key={index}>
-      <li>{name}</li>
-      <li>email</li>
-      <li>status</li>
-    </div>
+    <ul className="grid grid-cols-6 border-b-slate-700 border-b p-2 hover:bg-smoke-4" key={index}>
+      <li className="col-span-2">{name}</li>
+      <li className="col-span-3">email</li>
+      <li className="col-span-1">status</li>
+    </ul>
   );
 };
 
