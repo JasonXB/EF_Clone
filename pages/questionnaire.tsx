e3f1ed580db6658ca5b1aed0b52e0f067744750b
vincecustomz <vince.vuong@live.com>
2022-10-20 21:07:25 -0400

    background image wont load

--- pages/questionnaire.tsx
+++ pages/questionnaire.tsx
@@ -1,24 +1,26 @@
 import type { NextPage } from 'next';
 import Layout from '../src/components/Layout';
 // import Image from 'next/image';
-import background from '../src/assets/Beckground-for-homepage2.png';
+// import background from '../src/assets/Beckground-for-homepage2.png';
 import Button from '../src/components/buttons/reusable-buttons';
 
 const questionnaire: NextPage = ({}) => {
   return (
     <Layout>
       {/* <Image className="border-2" src={img} alt="background"/> */}
-      <div
-        className="text-center pt-[50px]"
+      {/* <div
+        className="text-center py-[200px]"
         style={{ backgroundImage: `url(${background})` }}
-      >
+      > */}
+
+      <div className="bg-[url{'../src/assets/Beckground-for-homepage2.png'}]">
         <span className="text-primary-1 text-[24px] font-semibold">
           Search for the perfect mentor for you!
         </span>
 
-        <div className='flex justify-center items-center py-[40px]'>
+        <div className='flex justify-center items-center py-[30px]'>
         <input
-          className="border-2 rounded-[23px] h-[36px] w-1/2 sm:w-[350px] mddrop-shadow-md text-[10px] ss:text-[16px] text-center"
+          className="border-2 rounded-[23px] h-[36px] w-1/2 sm:w-[350px] md:w-[500px] drop-shadow-md text-[10px] ss:text-[16px] text-center"
           placeholder="Skills, Mentor name, or Industries"
           type="text"
           name="searchBar"
