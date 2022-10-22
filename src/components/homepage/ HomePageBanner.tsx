507f7b69c72d0af39d7e10a82a60e0c44d33558d
Kelsey H <kels-hall23@hotmail.com>
2022-10-22 12:52:53 -0400

    fixed center align

--- src/components/homepage/ HomePageBanner.tsx
+++ src/components/homepage/ HomePageBanner.tsx
@@ -7,7 +7,7 @@ import style from '../../../styles/homePage.module.css';
 const HomePageBanner = () => (
   //mobile view still debatable
   <div className=" overflow-auto  my-8 mb-20  sm:mt-[5%]  lg:mt-[4%] xl:w-3/4 xl:mx-auto ">
-    <div className="flex flex-col items-center w-full text-center md:block ss:py-4 ss:text-left">
+    <div className="flex flex-col items-center w-full text-center md:block ss:py-4 md:text-left">
       <div
         className={`${style.customClipPath} ss:w-[80%] md:w-[60%] lg:w-[70%] float-right w-full  max-w-[1000px]`}
       >
@@ -22,7 +22,7 @@ const HomePageBanner = () => (
         <h1 className="mt-4 text-3xl font-semibold leading-tight sm:text-5xl text-primary-1 lg:text-6xl">
           Maximizing Hope, <br /> Empowering Potential
         </h1>
-        <p className="text-lg ss:w-9/12 ss:py-4 md:text-xl">
+        <p className="py-4 text-lg ss:w-3/4 ss:mx-auto md:mx-0 md:w-9/12 md:text-xl">
           We help empowered young minds to look forward to their future.
           We&lsquo;re looking for mentors ready to support the next generation
           of leaders
