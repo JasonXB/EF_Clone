91af51d7887c6ab5d9f04eaea606c392a8adb049
Ricardo Maharaj <ricardorobinmaharaj@gmail.com>
2022-10-23 02:07:20 -0400

    requested changes have been made

--- pages/notifications.tsx
+++ pages/notifications.tsx
@@ -1,18 +1,28 @@
 import Layout from '../src/components/Layout';
+import NotificationCard from '../src/components/notificationCard';
+import { UserNotification } from '../src/interface/user-notification.interface';
+import { NextPage } from 'next';
+import { useEffect, useState } from 'react';
 
-const TEMP_NOTIFICATIONS = new Array(3).fill({
-  date: new Date(),
-  name: 'John Doe',
-  profile_path: '',
-  country: 'Canada',
-  position: 'Example Position',
-  body: 'You have a new notification',
-});
+const Notifications: NextPage = () => {
+  let [notifications, setNotifications] = useState<UserNotification[]>();
 
-function Notifications() {
+  useEffect(() => {
+    setNotifications(
+      new Array(3).fill({
+        date: new Date(),
+        name: 'John Doe',
+        profile_path: '',
+        country: 'Canada',
+        position: 'Example Position',
+        body: 'You have a new notification',
+      })
+    );
+  }, []);
+
   return (
     <>
-      <Layout headTitle="Notifications | Empowered Futures">
+      <Layout headTitle="Notifications">
         <div className="container mx-auto">
           <div className="flex flex-col space-y-2">
             <div className="flex flex-row justify-center">
@@ -20,42 +30,18 @@ function Notifications() {
                 {'Notifications'}
               </span>
             </div>
-            {TEMP_NOTIFICATIONS.map((notification, i) => (
-              <div
-                className="flex flex-row px-4 py-8 border rounded-[38px]"
-                style={{
-                  boxShadow: '0px 0px 10px #00000054',
-                }}
-                key={`${i}-${notification.name}`}
-              >
-                <div className="flex flex-col mr-2">
-                  <img
-                    className="rounded-xl max-w-[147px] max-h-[157px]"
-                    src="https://picsum.photos/200"
-                    width={147}
-                    height={157}
-                    alt="Placeholder Photo"
-                  />
-                </div>
-                <div className="flex flex-col space-y-1">
-                  <div className="text-sm">
-                    {notification.date.toLocaleDateString()}
-                  </div>
-                  <div className="text-sm">
-                    {notification.date.toLocaleTimeString()}
-                  </div>
-                  <div className="font-bold">{notification.name}</div>
-                  <div className="font-bold">{notification.country}</div>
-                  <div className="font-bold">{notification.position}</div>
-                  <div className="text-sm">{notification.body}</div>
-                </div>
-              </div>
-            ))}
+            {notifications &&
+              notifications.map((notification, i) => (
+                <NotificationCard
+                  notification={notification}
+                  key={`${i}-${notification.name}`}
+                />
+              ))}
           </div>
         </div>
       </Layout>
     </>
   );
-}
+};
 
 export default Notifications;
---
+++ src/components/notificationCard.tsx
@@ -0,0 +1,43 @@
+import { UserNotification } from '../interface/user-notification.interface';
+
+interface NotificationCardProps {
+  notification: UserNotification;
+}
+
+function NotificationCard({ notification }: NotificationCardProps) {
+  const date = notification.date;
+  const hours = notification.date.getHours().toString().padStart(2, '0');
+  const minutes = notification.date.getMinutes().toString().padStart(2, '0');
+  const timeNotation = date.toLocaleTimeString().slice(-2).toLocaleLowerCase();
+
+  return (
+    <>
+      <div
+        className="flex flex-row px-4 py-8 border rounded-[38px]"
+        style={{
+          boxShadow: '0px 0px 10px #00000054',
+        }}
+      >
+        <div className="flex flex-col mr-2">
+          <img
+            className="rounded-xl max-w-[147px] max-h-[157px]"
+            src="https://picsum.photos/200"
+            alt=""
+          />
+        </div>
+        <div className="flex flex-col space-y-1">
+          <div className="text-sm">
+            {notification.date.toLocaleDateString()}
+          </div>
+          <div className="text-sm">{`${hours}:${minutes}${timeNotation}`}</div>
+          <div className="font-bold">{notification.name}</div>
+          <div className="font-bold text-sm">{notification.country}</div>
+          <div className="font-bold text-sm">{notification.position}</div>
+          <div className="text-sm">{notification.body}</div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default NotificationCard;
