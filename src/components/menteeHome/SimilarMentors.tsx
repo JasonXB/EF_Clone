b0c597e736971da482f32c81a85ade673d355b3b
JasonXB <jasonxportfolio@gmail.com>
2022-10-23 23:28:42 -0400

    Adding text content to mentee home carousel

--- src/components/menteeHome/SimilarMentors.tsx
+++ src/components/menteeHome/SimilarMentors.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { Splide, SplideSlide, SplideTrack } from '@splidejs/react-splide';
 import Image from 'next/image';
 import Hiba from '../../../public/assets/hiba.png';
+import BubbleTag from '../BubbleTag';
 import '@splidejs/react-splide/css';
+import { BUBBLE_TAG_TYPE_CLASSES } from '../BubbleTag';
 
 export default function SimilarMentors() {
   const Options = {
@@ -26,7 +28,7 @@ export default function SimilarMentors() {
 
   const comp = (
     <SplideSlide className="slide px-8 ad:px-0">
-      <div className="grid grid-cols-[40%_1fr] h-[14.6875rem] p-6 bg-emerald-200 rounded-[20px]">
+      <div className="grid grid-cols-[40%_1fr] gap-4 h-[14.6875rem] p-6 bg-emerald-200 rounded-[20px]">
         <Image
           src={Hiba}
           height="100%"
@@ -34,6 +36,20 @@ export default function SimilarMentors() {
           alt=""
           className="rounded-[20px]"
         />
+        <div className="grid grid-rows-[auto_auto_auto_1fr_26px_26px] gap-2">
+          <h6 className="font-semibold mx-1]">Darra Wittney</h6>
+          <p className="font-medium mx-1">USA</p>
+          <p className="font-medium mx-1">Project Manager at CIBC</p>
+          <br />
+          <BubbleTag
+            tag="Entrepeneurship"
+            bubbleTagType={BUBBLE_TAG_TYPE_CLASSES.primaryLight}
+          />
+          <BubbleTag
+            tag="Management"
+            bubbleTagType={BUBBLE_TAG_TYPE_CLASSES.primaryLight}
+          />
+        </div>
       </div>
     </SplideSlide>
   );
