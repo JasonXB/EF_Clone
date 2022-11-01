a2b56db4eaadda3019c696fd0ee518bcdcac3aac
vincecustomz <vince.vuong@live.com>
2022-11-01 02:29:38 -0400

    MAGNIFYING GLASS FINALLY.

--- pages/questionnaire.tsx
+++ pages/questionnaire.tsx
@@ -13,32 +13,33 @@ const questionnaire: NextPage = ({}) => {
             Search for the perfect mentor for you!
           </span>
 
-          <div className="flex justify-center items-center py-[30px]">
-            <input
-              className="border-2 rounded-[23px] h-[36px] w-1/2 sm:w-[350px] md:w-[500px] drop-shadow-md text-[10px] ss:text-[16px] text-center"
+          <div className="flex justify-center items-center py-[30px] relative">
+             <input
+              className="border-2 rounded-[23px] h-[36px] w-[231px] sm:w-[350px] md:w-[500px] md:text-center drop-shadow-md text-[10px] sm:text-[16px] pl-[22px]"
               placeholder="Skills, Mentor name, or Industries"
               type="text"
               name="searchBar"
               required
               autoFocus
             ></input>
-            <svg
-              className="h-8 w-8 text-black"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              stroke-width="2"
-              stroke="currentColor"
-              fill="none"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            >
-              {' '}
-              <path stroke="none" d="M0 0h24v24H0z" />{' '}
-              <circle cx="10" cy="10" r="7" />{' '}
-              <line x1="21" y1="21" x2="15" y2="15" />
-            </svg>
-
+            <div className='absolute ml-20 sm:ml-48 md:ml-80'>
+              <svg
+                className="h-8 w-8 text-black"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                stroke-width="2"
+                stroke="currentColor"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                {' '}
+                <path stroke="none" d="M0 0h24v24H0z" />{' '}
+                <circle cx="10" cy="10" r="7" />{' '}
+                <line x1="21" y1="21" x2="15" y2="15" />
+              </svg>
+            </div>
             <Button
               variant="primary"
               type="submit"
