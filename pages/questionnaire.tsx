01fffe5f813bd37f1726ee9baf5c834ff56b8fe3
JasonXB <jasonxportfolio@gmail.com>
2022-11-02 13:56:27 -0400

    Minor questionnaire edits (Will touch up later)

--- pages/questionnaire.tsx
+++ pages/questionnaire.tsx
@@ -1,29 +1,26 @@
-import type { NextPage } from 'next';
 import Layout from '../src/components/Layout';
 import Button from '../src/components/buttons/reusable-buttons';
 // import SearchInput from '../src/components/SearchInput';
 
-const questionnaire: NextPage = ({}) => {
+const questionnaire = ({}) => {
   return (
     <Layout>
-      <div
-        className={`bg-[url(../src/assets/Beckground-for-homepage2.png)] bg-cover bg-opacity-[50%] text-center py-[200px]`}
-      >
+      <div className={`bg-cover bg-opacity-[50%] text-center py-[200px]`}>
         <div className="">
-          <span className="font-semibold text-primary-1 text-[24px] font-semibold">
+          <h3 className="text-primary-1 font-semibold">
             Search for the perfect mentor for you!
-          </span>
+          </h3>
 
-          <div className="flex justify-center items-center py-[30px] relative">
-             <input
-              className="border-2 rounded-[23px] h-[36px] w-[231px] sm:w-[350px] md:w-[500px] md:text-center drop-shadow-md text-[10px] sm:text-[16px] pl-[22px]"
+          <div className="flex justify-center items-center py-[30px] relative mr-[50px]">
+            <input
+              className=" border-2 rounded-[23px] h-[45px] w-[231px] sm:w-[350px] md:w-[500px] md:text-center drop-shadow-md text-[10px] sm:text-[16px] pl-[22px]"
               placeholder="Skills, Mentor name, or Industries"
               type="search"
               name="searchBar"
               required
               autoFocus
-            ></input>
-            <div className='absolute ml-20 sm:ml-48 md:ml-80'>
+            />
+            <div className="absolute ml-20 sm:ml-48 md:ml-80">
               <svg
                 className="h-8 w-8 text-black"
                 width="24"
