5eb0c0ee407358b66cf762596eee8cfed323e20c
Ricardo Maharaj <ricardorobinmaharaj@gmail.com>
2022-11-02 02:19:15 -0400

    /mentor/profile removed in favor of /mentor/:id

--- pages/mentor/[id].tsx
+++ pages/mentor/[id].tsx
@@ -1,25 +1,21 @@
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import Layout from '../../src/components/Layout';
-import MentorCard from '../../src/components/MentorCard'; // using MentorCard until full user profile is complete
-import { Mentor } from '../../src/interface/mentor.interface';
-import { TempMentorDB } from '../../src/tempData/TempMentorDB';
+import MentorProfileMain from '../../src/components/mentorProfile/MentorProfileMain';
+import Mentor from '../../src/interface/mentor.interface';
+import MockMentorDB from '../../src/tempData/MockMentorDB';
 
 export default function MentorProfile() {
   let router = useRouter();
   let { id } = router.query;
   let [mentor, setMentor] = useState<Mentor>();
   useEffect(() => {
-    setMentor(TempMentorDB.getByID(parseInt(`${id}`)));
+    setMentor(MockMentorDB.getByID(parseInt(`${id}`)));
   }, []);
   return (
     <>
-      <Layout>
-        <div className="container mx-auto">
-          <div className="flex flex-col items-center">
-            {mentor && <MentorCard mentor={mentor} />}
-          </div>
-        </div>
+      <Layout headTitle="Mentor">
+        {mentor && <MentorProfileMain mentor={mentor} />}
       </Layout>
     </>
   );
