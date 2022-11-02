feb6cc7fdf55c4043da8222122fc3930c8ce8463
JasonXB <jasonxportfolio@gmail.com>
2022-11-02 10:43:24 -0400

    Protected all pages in the Mentee folder

--- pages/mentee/congrats.tsx
+++ pages/mentee/congrats.tsx
@@ -1,49 +1,51 @@
-import type { NextPage } from 'next';
 import Link from 'next/link';
-import Router from 'next/router';
 import Layout from '../../src/components/Layout';
 import Image from 'next/image';
 import fireworks from '../../public/assets/fireworks.png';
 import Button from '../../src/components/buttons/reusable-buttons';
-import { ReactEventHandler } from 'react';
+import Router from 'next/router';
+import useAuthStatusCheck from '../../src/hooks/useAuthStatusCheck';
+import Spinner from '../../src/components/loadingVisuals/spinner';
 
-//! check whether a user is authenticated as a mentee (required to view this page)
-// otherwise user is redirected to /auth/login
-const congratulationsMentee: NextPage = ({}) => {
-  const clickHandler: ReactEventHandler = () => {
-    Router.push('/');
-  };
-  return (
-    <Layout>
-      <div className="flex flex-col md:flex-row justify-center items-center md:items-start">
-        <div className="object-contain max-w-[600px] flex justify-center items-center md:py-18">
-          <Image alt="illustration of fireworks" src={fireworks} />
-        </div>
-        <div className=" font-mainFont flex flex-col justify-center items-center text-center w-ss px-3 md:py-20">
-          <div className="font-bold text-primary-1 text-3xl sm:text-4xl lg:text-5xl ss:w-[500px] md:w-full">
-            CONGRATULATIONS ON BECOMING A MENTEE!
+const congratulationsMentee = ({}) => {
+  //# Check what the user is authrorized as (Guest, Mentee, Mentor, or Admin)
+  //# If the user is not a mentee, redirect them to /auth/login
+  const authStatus = useAuthStatusCheck();
+  if (authStatus === 'Loading') return <Spinner />;
+  if (authStatus !== 'Mentee') Router.replace('/auth/login');
+  else
+    return (
+      <Layout>
+        <div className="flex flex-col md:flex-row justify-center items-center md:items-start">
+          <div className="object-contain max-w-[600px] flex justify-center items-center md:py-18">
+            <Image alt="illustration of fireworks" src={fireworks} />
           </div>
-          <p className=" text-lg lg:text-2xl py-8 w-[330px] xs:w-[430px] lg:w-[600px]">
-            Your Mentorship request has been sent to the selected Mentor! <br />
-            <br />
-            Check your emails for if the Mentor has accepted your mentorship
-            request.
-            <br /> Once the Mentor has accepted your request, your can book a
-            meeting with them.
-          </p>
-          <Link href="/">
-            <Button
-              variant="primary"
-              clickHandler={clickHandler}
-              disabled={false}
-            >
-              Go to Homepage
-            </Button>
-          </Link>
+          <div className=" font-mainFont flex flex-col justify-center items-center text-center w-ss px-3 md:py-20">
+            <div className="font-bold text-primary-1 text-3xl sm:text-4xl lg:text-5xl ss:w-[500px] md:w-full">
+              CONGRATULATIONS ON BECOMING A MENTEE!
+            </div>
+            <p className=" text-lg lg:text-2xl py-8 w-[330px] xs:w-[430px] lg:w-[600px]">
+              Your Mentorship request has been sent to the selected Mentor!{' '}
+              <br />
+              <br />
+              Check your emails for if the Mentor has accepted your mentorship
+              request.
+              <br /> Once the Mentor has accepted your request, your can book a
+              meeting with them.
+            </p>
+            <Link href="/">
+              <Button
+                variant="primary"
+                clickHandler={() => Router.push('/')}
+                disabled={false}
+              >
+                Go to Homepage
+              </Button>
+            </Link>
+          </div>
         </div>
-      </div>
-    </Layout>
-  );
+      </Layout>
+    );
 };
 
 export default congratulationsMentee;
--- pages/mentee/index.tsx
+++ pages/mentee/index.tsx
@@ -5,9 +5,9 @@ import MeetingListItem from '../../src/components/menteeHome/MeetingListItem';
 import OutlinedButton from '../../src/components/menteeHome/OutlinedButton';
 import { MockData } from '../../src/interface/mentee/homepage';
 import useAuthStatusCheck from '../../src/hooks/useAuthStatusCheck';
+import Spinner from '../../src/components/loadingVisuals/spinner';
+import Router from 'next/router';
 
-//! check whether a user is authenticated as a mentee
-// required to view this page- otherwise user is redirected to /auth/login
 export default function Index() {
   // Mock data all grouped in the following objects
   const mock: MockData = {
@@ -46,74 +46,77 @@ export default function Index() {
     ],
   };
 
-  // Testing the authentication status check hook
+  //# Check what the user is authrorized as (Guest, Mentee, Mentor, or Admin)
+  //# If the user is not a mentee, redirect them to /auth/login
   const authStatus = useAuthStatusCheck();
-  console.log('HOOK RETURNS AUTH STATUS OF: ', authStatus);
-  return (
-    <Layout>
-      <h3 className="font-semibold text-center mt-10 text-primary-1">
-        Welcome back
-        <br className="sm:hidden" /> {mock.username}
-      </h3>
-      <section className="grid grid-cols-1 lg:grid-cols-2 sm:max-w-[80%] sm:mx-auto lg:max-w-full lg:divide-x">
-        <div className="lg:pr-5 mt-10">
-          <h4 className="text-center mb-6 text-primary-1 font-bold">
-            My Applications
-          </h4>
-          {mock.applications.length > 0 ? (
-            mock.applications.map((el, i: number) => {
-              if (i > 2) return; // render a max of 3
-              return (
-                <ApplicationListItem
-                  key={i}
-                  mentorName={el.mentorName}
-                  status={el.status}
-                />
-              );
-            })
-          ) : (
-            <h6 className="text-center mt-10">No applications!</h6>
-          )}
-          {mock.applications.length > 3 && (
-            <OutlinedButton text="See all applications" onClick={() => {}} />
-          )}
-        </div>
-        <div className="lg:pl-5 mt-10">
-          <h4 className="text-center mb-6 text-primary-1 font-bold">
-            Upcoming Meetings
-          </h4>
-          <ul>
-            {mock.meetings.length > 0 ? (
-              mock.meetings.map((el, i: number) => {
+  if (authStatus === 'Loading') return <Spinner />;
+  if (authStatus !== 'Mentee') Router.replace('/auth/login');
+  else
+    return (
+      <Layout>
+        <h3 className="font-semibold text-center mt-10 text-primary-1">
+          Welcome back
+          <br className="sm:hidden" /> {mock.username}
+        </h3>
+        <section className="grid grid-cols-1 lg:grid-cols-2 sm:max-w-[80%] sm:mx-auto lg:max-w-full lg:divide-x">
+          <div className="lg:pr-5 mt-10">
+            <h4 className="text-center mb-6 text-primary-1 font-bold">
+              My Applications
+            </h4>
+            {mock.applications.length > 0 ? (
+              mock.applications.map((el, i: number) => {
                 if (i > 2) return; // render a max of 3
                 return (
-                  <MeetingListItem
+                  <ApplicationListItem
                     key={i}
                     mentorName={el.mentorName}
-                    mentorPosition={el.mentorPosition}
-                    date={el.date}
-                    time={el.time}
+                    status={el.status}
                   />
                 );
               })
             ) : (
-              <h6 className="text-center mt-10">No meetings planned!</h6>
+              <h6 className="text-center mt-10">No applications!</h6>
             )}
-          </ul>
-          {mock.meetings.length > 3 && (
-            <OutlinedButton text="See all meetings" onClick={() => {}} />
-          )}
-        </div>
-      </section>
-      <section className="bg-[#F2F2F2] mt-16">
-        <h4 className="text-center">
-          We think these mentors are a good match for you.
-        </h4>
-        <h6 className="text-center">
-          Will add a carousel after this gets merged into development
-          <br /> Would rather not set up Splide when someone else already has
-        </h6>
-      </section>
-    </Layout>
-  );
+            {mock.applications.length > 3 && (
+              <OutlinedButton text="See all applications" onClick={() => {}} />
+            )}
+          </div>
+          <div className="lg:pl-5 mt-10">
+            <h4 className="text-center mb-6 text-primary-1 font-bold">
+              Upcoming Meetings
+            </h4>
+            <ul>
+              {mock.meetings.length > 0 ? (
+                mock.meetings.map((el, i: number) => {
+                  if (i > 2) return; // render a max of 3
+                  return (
+                    <MeetingListItem
+                      key={i}
+                      mentorName={el.mentorName}
+                      mentorPosition={el.mentorPosition}
+                      date={el.date}
+                      time={el.time}
+                    />
+                  );
+                })
+              ) : (
+                <h6 className="text-center mt-10">No meetings planned!</h6>
+              )}
+            </ul>
+            {mock.meetings.length > 3 && (
+              <OutlinedButton text="See all meetings" onClick={() => {}} />
+            )}
+          </div>
+        </section>
+        <section className="bg-[#F2F2F2] mt-16">
+          <h4 className="text-center">
+            We think these mentors are a good match for you.
+          </h4>
+          <h6 className="text-center">
+            Will add a carousel after this gets merged into development
+            <br /> Would rather not set up Splide when someone else already has
+          </h6>
+        </section>
+      </Layout>
+    );
 }
