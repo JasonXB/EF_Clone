789f54973786374317564c52211527481f05ad57
joshuapadronuy <joshuapadronuy@gmail.com>
2022-11-03 14:51:12 -0700

    change adminPanel from component testing to e2e testing

--- cypress/components/achievementCircle/achievementCircle.cy.tsx
+++ cypress/components/achievementCircle/achievementCircle.cy.tsx
@@ -4,7 +4,7 @@ import Cat from '../../../public/assets/cat.jpeg';
 describe('achievementCircle.cy.tsx', () => {
   const testText = 'foo bar baz';
   it('should load with text when the text prop is supplied', () => {
-    cy.mount(<AchievementCircle text={testText} progress={50} image={Cat} />);
+    cy.mount(<AchievementCircle text={testText} progress={10} image={Cat} />);
     cy.get('.CircularProgressbar-text').should('have.text', testText);
   });
 });
--- cypress/e2e/adminPanel.cy.tsx
+++ cypress/e2e/adminPanel.cy.tsx
@@ -1,20 +1,15 @@
-import AdminPanelDashboard from '../../../pages/admin-panel';
-
-import { placeholderDataForRequest as dummyMentors } from '../../../src/tempData/temp-data-mentor';
+import { placeholderDataForRequest as dummyMentors } from '../../src/tempData/temp-data-mentor';
 
 describe('adminPanel.cy.tsx', () => {
-  const testText = 'foo bar baz';
-  it('should mount', () => {
-    cy.mount(<AdminPanelDashboard />);
-  });
+  beforeEach(()=>{
+    cy.visit(`http://localhost:3000/admin-panel`)
+  })
   it('should load one entry for each dummy mentor', () => {
-    cy.mount(<AdminPanelDashboard />);
     cy.get('#applicantList')
       .children()
       .should('have.length', dummyMentors.length);
   });
   it('should have one applicantName div for each dummy, with the applicants name', () => {
-    cy.mount(<AdminPanelDashboard />);
     cy.get('#applicantList')
       .children()
       .each((element, index) => {
