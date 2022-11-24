adc43a540fc61983cf942b32752e1d6616be5c64
joshuapadronuy <joshuapadronuy@gmail.com>
2022-11-23 16:49:57 -0800

    removed console log

--- src/api/meetings/post-meeting.ts
+++ src/api/meetings/post-meeting.ts
@@ -4,24 +4,22 @@ import { getCredentialsFromLocalStorage } from '../../api/localStorage';
 
 export async function postMeeting (meeting: MeetingData) {
   
-    const accessToken = getCredentialsFromLocalStorage();
+  const accessToken = getCredentialsFromLocalStorage();
 
-    const config = {
-      headers: { 
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${accessToken}` }
-    };
+  const config = {
+    headers: { 
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${accessToken}` }
+  };
 
-    await axios.post(
-        'https://efback.azurewebsites.net/api/meeting/auth/set_meeting/',
-        meeting,
-        config
-    ).then((res) => {
-      console.log(res.data);
-    }).catch((err)=> {
-      console.error(err);
-    })
+  await axios.post(
+      'https://efback.azurewebsites.net/api/meeting/auth/set_meeting/',
+      meeting,
+      config
+  ).catch((err)=> {
+    console.error(err);
+  })
 
-    //need to delete the meeting time which is booked from availability and update data (DELETE - availability, if a specific date)
+  //need to delete the meeting time which is booked from availability and update data (DELETE - availability, if a specific date)
 
   };
