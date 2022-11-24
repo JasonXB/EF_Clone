e6dda9151f2492c7602029b1efa2c3538349fb25
Kelsey H <kels-hall23@hotmail.com>
2022-11-24 16:42:38 -0500

    basic letter added with read more

---
+++ src/components/homepage/LetterFromHiba/Letter.tsx
@@ -0,0 +1,53 @@
+import Image from 'next/image';
+import { useState } from 'react';
+import letterContent from './LetterContent';
+
+const Letter = () => {
+  let split = letterContent.content.split('\n'); //split up paragraphs
+  const [isReadMore, setIsReadMore] = useState(true);
+  const toggleReadMore = () => {
+    setIsReadMore(!isReadMore);
+  };
+  return (
+    <section className="max-w-5xl py-10 mx-auto rounded-lg bg-hue-200 drop-shadow-lg">
+      <div className="max-w-4xl p-4 mx-auto ">
+        <h4 className="my-2 text-primary-1">A Note From Our Founder</h4>
+        <div className="my-4 ">
+          <h5 className="text-xl italic">
+            Help me make a powerful contribution to the youth of the new world.
+          </h5>
+          <div
+            className={`${isReadMore ? 'max-h-[510px]' : ' '} overflow-hidden`}
+          >
+            {split.map((para, i) => (
+              <p key={i} className="my-4 text-lg italic">
+                {para}
+              </p>
+            ))}
+          </div>
+
+          <span onClick={toggleReadMore} className="read-or-hide">
+            {isReadMore ? '...read more' : ' show less'}
+          </span>
+        </div>
+        <div className="flex justify-end w-full">
+          <div className="flex flex-row items-center">
+            <span className="flex flex-col mr-4 text-right">
+              <p className="text-lg font-bold">Hiba Badran</p>
+
+              <p className="italic">Chairperson Of The Board</p>
+            </span>
+            <Image
+              src={letterContent.img}
+              alt="Hiba"
+              width={150}
+              height={150}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Letter;
