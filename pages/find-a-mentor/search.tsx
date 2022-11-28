6ca6b4ff836dd5b74583aa03d14125cbfeed6e3c
joshuapadronuy <joshuapadronuy@gmail.com>
2022-11-27 17:21:36 -0800

    111122-loadMentors

--- pages/find-a-mentor/search.tsx
+++ pages/find-a-mentor/search.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 import MentorCard from '../../src/components/MentorCard';
 import Mentor from '../../src/interface/mentor.interface';
 import { useRouter } from 'next/router';
-import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+import { getMentors } from '../../src/api/mentorList/mentor-list';
 
 enum FilterDefaults {
   Gender = 'All',
@@ -48,56 +48,15 @@ export default function MentorList() {
   let [query, setQuery] = useState(q as string);
   let [page, setPage] = useState(1);
 
-  //
-  const getMentors = async () => {
-    const options: AxiosRequestConfig = {
-      name: "Get all mentors' profile",
-      id: '57c19079-77fa-4aad-9ce3-1d93525b1ef2',
-      protocolProfileBehavior: {
-        disableBodyPruning: true,
-      },
-      request: {
-        method: 'GET',
-        header: [],
-        url: 'https://efback.azurewebsites.net/api/mentor/list/all',
-      },
-      response: [],
-    };
 
-    try {
-      const response: AxiosResponse = await axios.get(
-        'https://efback.azurewebsites.net/api/mentor/list/all',
-        options
-      );
-      // new array of Mentor objects created to change backend property names to match frontend types
-      let newMentors: any = [];
-      response.data.mentors.map((mentor: any) => {
-        const newMentor: Mentor = {
-          id: mentor._id,
-          first_name: mentor.firstname,
-          last_name: mentor.lastname,
-          location: mentor.address,
-          gender: mentor.gender,
-          profile_path: mentor.picture,
-          job: mentor.title,
-          bio: mentor.bio,
-          email: mentor.email,
-          tags: mentor.fields,
-          skills: mentor.skillsets,
-        };
-        newMentors.push(newMentor);
-      });
-
-      setAllMentors(newMentors);
-      return response;
-    } catch (error) {
-      console.log(error);
-    }
-  };
 
   // on page render, get Mentors from backend
   useEffect(() => {
-    getMentors();
+    const getAllMentors = async () => {
+      const mentors = await getMentors()
+      setAllMentors(mentors as Mentor[])
+    }
+    getAllMentors();
   }, []);
 
   // gender filtering
